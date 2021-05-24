e5597efe9faca947c6de189bcbf904a1f5e636e7
Gavisann <richylombano@gmail.com>
2021-05-24 10:54:04 -0400

    updating plugin

--- plugins/tobefair.py
+++ plugins/tobefair.py
@@ -3,15 +3,15 @@ import re
 
 
 @hook.command("tobefair", autohelp=False)
-def bonk( nick, chan, db, notice):
+def tobefair( nick, chan, db, notice):
     """no useful help txt"""
-    return "https://c.tenor.com/rwplrVlY2xoAAAAM/letterkenny-to-be-fair.gif"
+    return "https://usercontent.irccloud-cdn.com/file/SVAKkvZ7/letterkenny-to-be-fair.gif"
 
 
-message = re.compile('t*o* b*e* f*a*i*r*',re.IGNORECASE)
+message = re.compile('t+o+ b+e+ f+a+i+r+',re.IGNORECASE)
 
 
 @hook.regex(message)
-def wellcool(match, nick, chan, db, notice):
+def tobefair_regex(match, nick, chan, db, notice):
     """no useful help txt"""
-    return "https://c.tenor.com/rwplrVlY2xoAAAAM/letterkenny-to-be-fair.gif"
+    return "https://usercontent.irccloud-cdn.com/file/SVAKkvZ7/letterkenny-to-be-fair.gif"
